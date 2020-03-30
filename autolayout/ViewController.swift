60b3b4ca96d907cd2934f68f33c410e699334016
sakaiatuki <atk721@icloud.com>
2020-03-30 14:49:00 +0900

    Initial Commit

---
+++ autolayout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  autolayout
+//
+//  Created by 酒井専冴 on 2020/03/30.
+//  Copyright © 2020 sakai_atsuki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
